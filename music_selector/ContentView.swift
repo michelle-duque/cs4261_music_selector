30a635ba7164a8f134c630ac24578eb8dd2a1c3c
Couch Lemon <michellemaya48@gmail.com>
2024-01-28 23:18:19 -0500

    initial commit

---
+++ music_selector/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  music_selector
+//
+//  Created by Michelle Duque on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ music_selector/music_selectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_selectorApp.swift
+//  music_selector
+//
+//  Created by Michelle Duque on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct music_selectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
